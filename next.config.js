a43aae2a216459f7f0741933802343d4c9a9a5d9
baymaxfpo <billmark0093@gmail.com>
2025-05-10 17:10:52 -0400

    initial commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "pino-pretty": false,
+      "encoding": false,
+    };
+    return config;
+  },
+}
+
+module.exports = nextConfig
